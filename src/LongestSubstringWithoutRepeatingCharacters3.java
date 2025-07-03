0b351741b4411716db873e05dbe4b2e41757c8eb
ashrith-UMB <ashrith.ponugoti001@umb.edu>
2025-07-03 12:03:20 -0400

    Add Corrected LongestSubstringWithoutRepeatingCharacters3

--- src/LongestSubstringWithoutRepeatingCharacters3.java
+++ src/LongestSubstringWithoutRepeatingCharacters3.java
@@ -3,10 +3,12 @@ import java.util.Set;
 
 public class LongestSubstringWithoutRepeatingCharacters3 {
     public int lengthOfLongestSubstring(String s) {
+
+
+
+        /* Wrong approach - goes wrong for string "dvdf"
         Set<Character> stringSet = new HashSet<Character>();
         int count = 0, max = 0;
-
-
         for(int i = 0; i < s.length(); i++){
             if(stringSet.contains(s.charAt(i))){
                 if(count > max)
@@ -20,9 +22,22 @@ public class LongestSubstringWithoutRepeatingCharacters3 {
             stringSet.add(s.charAt(i));
             count++;
         }
-        return Math.max(count, max);
+        return Math.max(count, max);*/
 
-        //return max;
+        //SLIDING WINDOW APPROACH
+        Set<Character> charSet = new HashSet<Character>();
+        int max = 0, left = 0;
+
+        for(int right = 0; right < s.length(); right++){
+            while(charSet.contains(s.charAt(right))){
+                charSet.remove(s.charAt(left));
+                left++;
+            }
+            charSet.add(s.charAt(right));
+            max = Math.max(max, right - left + 1);
+        }
+
+        return max;
     }
 
     public static void main(String[] args){
@@ -32,8 +47,8 @@ public class LongestSubstringWithoutRepeatingCharacters3 {
 
         LongestSubstringWithoutRepeatingCharacters3 checker = new LongestSubstringWithoutRepeatingCharacters3();
 
-        //System.out.println("Result of s1: "+s1+" "+ checker.lengthOfLongestSubstring(s1));
-        //System.out.println("Result of s2: "+s2+" "+ checker.lengthOfLongestSubstring(s2));
+        System.out.println("Result of s1: "+s1+" "+ checker.lengthOfLongestSubstring(s1));
+        System.out.println("Result of s2: "+s2+" "+ checker.lengthOfLongestSubstring(s2));
         System.out.println("Result of s3: "+s3+" "+ checker.lengthOfLongestSubstring(s3));
 
     }
