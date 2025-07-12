2bf7dad0514e68b441ed737d0c9ece3064d0ed6a
ashrith-UMB <ashrith.ponugoti001@umb.edu>
2025-07-12 01:49:26 -0400

    Add TopKFrequentElements347 with O(nlogn) time complexity

--- src/TopKFrequentElements347.java
+++ src/TopKFrequentElements347.java
@@ -1,5 +1,6 @@
 /*Top K Frequent Elements
-        Given an integer array nums and an integer k, return the k most frequent elements within the array.
+        Given an integer array nums and an integer k, return the k most frequent elements
+        within the array.
 
         The test cases are generated such that the answer is always unique.
 
@@ -21,22 +22,66 @@
         -1000 <= nums[i] <= 1000
         1 <= k <= number of distinct elements in nums.*/
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class TopKFrequentElements347 {
     public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> countFrequentElements = new HashMap<>();
+        Arrays.sort(nums);
 
-        return new int[]{};
+        for(int num : nums){
+            if(countFrequentElements.containsKey(num)){
+                int currentCount = countFrequentElements.get(num);
+                countFrequentElements.replace(num, currentCount, currentCount + 1);
+            }else {
+                countFrequentElements.put(num, 1);
+            }
+        }
+        ArrayList<Integer> resultArray = new ArrayList<>();
+
+        for(int key : countFrequentElements.keySet()){
+            if(countFrequentElements.get(key) >= k)
+                resultArray.add(key);
+        }
+
+        int[] result = new int[resultArray.size()];
+        for(int i = 0; i < resultArray.size(); i++){
+            result[i] = resultArray.get(i);
+        }
+
+        return result;
     }
 
     public static void main(String[] args){
-        int[] array1 = new int[]{1,2,2,3,3,3};
+        int[] array1 = new int[]{1,1,1,1,2,2,3,3,3};
         int[] array2 = new int[]{7,7};
         int[] array3 = new int[]{};
 
         TopKFrequentElements347 checker = new TopKFrequentElements347();
 
-        System.out.println("Result for top 2 elements of {1,2,2,3,3,3} : "+ checker.topKFrequent(array1,2));
-        System.out.println("Result for top 1 elements of {7,7} : "+ checker.topKFrequent(array2,1));
-        System.out.println("Result for top 1 elements of {} : "+ checker.topKFrequent(array3,1));
+        System.out.println("Result for top 3 elements of {1,1,1,1,2,2,3,3,3} : ");
+        int[] resultArray = checker.topKFrequent(array1,3);
+        System.out.print("[");
+        for(int i = 0; i < resultArray.length; i++){
+            System.out.print(resultArray[i]+",");
+        }
+        System.out.println("]");
+        System.out.println("Result for top 1 elements of {7,7} : ");
+        resultArray = checker.topKFrequent(array2,1);
+        System.out.print("[");
+        for(int i = 0; i < resultArray.length; i++){
+            System.out.print(resultArray[i]+",");
+        }
+        System.out.println("]");
+        System.out.println("Result for top 1 elements of {} : ");
+        resultArray = checker.topKFrequent(array3,1);
+        System.out.print("[");
+        for(int i = 0; i < resultArray.length; i++){
+            System.out.print(resultArray[i]+",");
+        }
+        System.out.println("]");
 
 
     }
